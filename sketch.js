140582825a01535b9d703e2afcfed5d52651307b
tomoyok216 <50037412+tomoyok216@users.noreply.github.com>
2020-11-09 18:05:53 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -379,9 +379,17 @@ class AnimationState extends State {
 //     }
     this.exactsW = windowWidth * 0.2;
     this.exacteW = windowWidth * 0.3;
-    this.nowW = 3.0 * (timewitha[this.count] - 1.0);
-    this.sW = windowWidth * (0.2 - this.nowW);
-    this.eW = windowWidth * (0.3 + this.nowW);
+    if(timewitha[this.count] > 1.0){
+      this.nowW = 3.0 * (timewitha[this.count] - 1.0);
+      this.sW = windowWidth * (0.2 - this.nowW);
+      this.eW = windowWidth * (0.3 + this.nowW);
+    }
+    if(timewitha[this.count] < 1.0){
+      this.nowW = 3.0 * (1.0 - timewitha[this.count]);
+      this.sW = windowWidth * (0.2 + this.nowW);
+      this.eW = windowWidth * (0.3 - this.nowW);
+    }
+    
     fill(210, 255, 255);
     rect(this.sW, 0, this.eW-this.sW, windowHeight);
     fill(11, 255, 255);
